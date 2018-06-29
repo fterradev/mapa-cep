a564e8a2553baedcadd7674786ea6ff188842e6a
Fernando Terra <nandobrt@gmail.com>
2018-06-29 20:12:51 -0300

    fix: throw reject reason instead of returning it

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-throw-literal */
 import fetchJsonp from 'fetch-jsonp';
 
 export const lookupLocation = cep => {
@@ -6,24 +7,24 @@ export const lookupLocation = cep => {
     .then(response => response.json())
     .then(data => {
       if (data.erro) {
-        throw 'CEP inexistente'; // eslint-disable-line no-throw-literal
+        throw 'CEP inexistente';
       }
       return data;
     })
     .catch(reason => {
       if (typeof reason === 'string') {
-        return reason;
+        throw reason;
       }
       if (reason.message.match(/failed *$/)) {
-        return 'Formato inválido de CEP (deve conter 8 dígitos)';
+        throw 'Formato inválido de CEP (deve conter 8 dígitos)';
       }
       if (reason.message.match(/timed out *$/)) {
-        return 'O serviço de CEP demorou muito para responder';
+        throw 'O serviço de CEP demorou muito para responder';
       }
       if (process.env.NODE_ENV !== 'production') {
-        return reason;
+        throw reason;
       } else {
-        return 'Erro desconhecido no acesso ao serviço de CEP';
+        throw 'Erro desconhecido no acesso ao serviço de CEP';
       }
     });
 };
