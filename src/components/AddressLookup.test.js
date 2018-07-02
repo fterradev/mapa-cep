66191cf99123af28e9798c8e7c93c9d293bc38bb
Fernando Terra <nandobrt@gmail.com>
2018-07-02 17:02:22 -0300

    test: Fix test description

--- src/components/AddressLookup.test.js
+++ src/components/AddressLookup.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import AddressLookup from './AddressLookup';
 
-describe('Wallet', () => {
+describe('AddressLookup', () => {
   const mockRenderResult = jest.fn();
   const addressLookup = shallow(<AddressLookup renderResult={mockRenderResult} />);
 
