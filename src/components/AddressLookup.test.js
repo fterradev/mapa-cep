18ce135f42ab5dfcc18a8297df6434a5e0ecf533
Fernando Terra <nandobrt@gmail.com>
2018-07-03 14:46:00 -0300

    test: assure findLocation is called

--- src/components/AddressLookup.test.js
+++ src/components/AddressLookup.test.js
@@ -4,9 +4,33 @@ import AddressLookup from './AddressLookup';
 
 describe('AddressLookup', () => {
   const mockRenderResult = jest.fn();
-  const addressLookup = shallow(<AddressLookup renderResult={mockRenderResult} />);
+  const addressLookup = shallow(
+    <AddressLookup renderResult={mockRenderResult} />
+  );
 
   it('renders properly', () => {
     expect(addressLookup).toMatchSnapshot();
+  });
+
+  describe('when the user submits a valid zip code', () => {
+    const cep = '02050-010';
+
+    beforeEach(() => {
+      addressLookup.find('Input').simulate('change', {
+        target: {
+          value: cep,
+          validity: {},
+          setCustomValidity: () => {}
+        }
+      });
+      addressLookup.find('Form').simulate('submit');
+    });
+    const findLocationSpy = jest.spyOn(
+      addressLookup.instance(),
+      'findLocation'
+    );
+    it('calls `this.findLocation`', () => {
+      expect(findLocationSpy).toHaveBeenCalled();
+    });
   });
 });
